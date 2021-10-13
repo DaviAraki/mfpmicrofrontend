9db8ca4761fd7987f0f8850e57bf3cf5a1a78429
Davi <davi.araki@gmail.com>
2021-10-13 16:21:12 -0300

    Fix config

--- packages/auth/config/webpack.dev.js
+++ packages/auth/config/webpack.dev.js
@@ -1,10 +1,14 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8082/',
+  },
   devServer: {
     port: 8082,
     historyApiFallback: true,
@@ -17,6 +21,9 @@ const devConfig = {
         './AuthApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
     }),
   ],
 };
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,10 +1,14 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8081/',
+  },
   devServer: {
     port: 8081,
     historyApiFallback: true,
@@ -17,6 +21,9 @@ const devConfig = {
         './MarketingApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
     }),
   ],
 };
